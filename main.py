599a0c901647e6d7d7fe257d27c9c52cb4739fa8
YOUMBI Harold <80352823+Youmbi-Harold@users.noreply.github.com>
2022-03-30 08:26:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+fruits = ["Apple","Peach","Pear"]#list of fruits
+for fruit in fruits: #setting up loop
+  print(fruit + 'pie' + "\n")#looping around the list fruits
+  print(fruits)#printing the list after any iteration
+print(fruits)#printing the list once
